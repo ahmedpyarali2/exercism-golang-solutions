d52b00924d9ed3d6cbd4ff5274a63b58d8aa5359
Ahmed Pyar Ali <ahmed.dhanani26@gmail.com>
2021-06-27 15:24:41 +0500

    add comments to solution

--- raindrops/raindrops.go
+++ raindrops/raindrops.go
@@ -9,15 +9,23 @@ import (
 // corresponding to certain potential factors.
 func Convert(number int) string {
 	raindropSound := ""
+
+	// add a "Pling" sound if the number is a factor of 3
 	if number%3 == 0 {
 		raindropSound += "Pling"
 	}
+
+	// add a "Plang" sound if the number is a factor of 5
 	if number%5 == 0 {
 		raindropSound += "Plang"
 	}
+
+	// add a "Plong" sound if the number is a factor of 7
 	if number%7 == 0 {
 		raindropSound += "Plong"
 	}
+
+	// add the number itself if the number is not a factor of either 3, 5 or 7
 	if raindropSound == "" {
 		raindropSound += strconv.Itoa(number)
 	}
