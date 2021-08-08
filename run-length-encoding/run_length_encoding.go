98ff6a774c65f18561f8eae87d893de1668f6ab6
Ahmed Pyar Ali <ahmed.ali@securiti.ai>
2021-08-09 01:29:49 +0500

    run length encode/decode completed

--- run-length-encoding/run_length_encoding.go
+++ run-length-encoding/run_length_encoding.go
@@ -5,6 +5,7 @@ package encode
 import (
 	"bytes"
 	"fmt"
+	"strconv"
 )
 
 // RunLengthEncode encodes the provided data using
@@ -26,10 +27,10 @@ func RunLengthEncode(data string) string {
 		result.WriteByte(char)
 	}
 
-	for i:=1; i<dataLen; i++ {
+	for i := 1; i < dataLen; i++ {
 		count++
 
-		if i == dataLen - 1 {
+		if i == dataLen-1 {
 			if data[i] != prev {
 				appendEncoding(prev, count)
 				count = 0
@@ -46,11 +47,51 @@ func RunLengthEncode(data string) string {
 	return result.String()
 }
 
-
 // RunLengthDecode decodes the provided data using
 // the run length decoding method.
 // NOTE: This method assumes the input will only contain
 // characters from a-z and A-Z along with space.
 func RunLengthDecode(data string) string {
-	return ""
+	if data == "" {
+		return ""
+	}
+
+	var (
+		result             bytes.Buffer
+		digitStartPosition int
+	)
+
+	// declare a append function to append character to the
+	// result string given (N) number of times
+	appendDecoding := func(char byte, times int) {
+		for i := 0; i < times; i++ {
+			result.WriteByte(char)
+		}
+	}
+
+	for i := 0; i < len(data); i++ {
+		currentByte := data[i]
+
+		// check if the current byte is a digit
+		if '0' <= currentByte && currentByte <= '9' {
+			continue
+		} else {
+			// current byte is not a digit
+			// check if difference between last digit position and
+			// current position is greater than 0
+			if digitStartPosition >= 0 && digitStartPosition-i < 0 {
+				// if it is we convert eh slice from `digitStartPosition` till current -1 into an
+				// integer and then append the current bytes, that many times to the resulting string
+				digit, _ := strconv.Atoi(data[digitStartPosition:i])
+				appendDecoding(currentByte, digit)
+			} else {
+				// else we append once
+				appendDecoding(currentByte, 1)
+			}
+
+			// also we set the `digitStartPosition` to the very next byte in the data
+			digitStartPosition = i + 1
+		}
+	}
+	return result.String()
 }
--- run-length-encoding/run_length_encoding_test.go
+++ run-length-encoding/run_length_encoding_test.go
@@ -11,29 +11,39 @@ func TestRunLengthEncode(t *testing.T) {
 		t.Logf("PASS RunLengthEncode - %s", test.description)
 	}
 }
-//func TestRunLengthDecode(t *testing.T) {
-//	for _, test := range decodeTests {
-//		if actual := RunLengthDecode(test.input); actual != test.expected {
-//			t.Errorf("FAIL %s - RunLengthDecode(%s) = %q, expected %q.",
-//				test.description, test.input, actual, test.expected)
-//		}
-//		t.Logf("PASS RunLengthDecode - %s", test.description)
-//	}
-//}
-//func TestRunLengthEncodeDecode(t *testing.T) {
-//	for _, test := range encodeDecodeTests {
-//		if actual := RunLengthDecode(RunLengthEncode(test.input)); actual != test.expected {
-//			t.Errorf("FAIL %s - RunLengthDecode(RunLengthEncode(%s)) = %q, expected %q.",
-//				test.description, test.input, actual, test.expected)
-//		}
-//		t.Logf("PASS %s", test.description)
-//	}
-//}
+
+func TestRunLengthDecode(t *testing.T) {
+	for _, test := range decodeTests {
+		if actual := RunLengthDecode(test.input); actual != test.expected {
+			t.Errorf("FAIL %s - RunLengthDecode(%s) = %q, expected %q.",
+				test.description, test.input, actual, test.expected)
+		}
+		t.Logf("PASS RunLengthDecode - %s", test.description)
+	}
+}
+
+func TestRunLengthEncodeDecode(t *testing.T) {
+	for _, test := range encodeDecodeTests {
+		if actual := RunLengthDecode(RunLengthEncode(test.input)); actual != test.expected {
+			t.Errorf("FAIL %s - RunLengthDecode(RunLengthEncode(%s)) = %q, expected %q.",
+				test.description, test.input, actual, test.expected)
+		}
+		t.Logf("PASS %s", test.description)
+	}
+}
 
 func BenchmarkEncode(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		for _, tt := range encodeTests {
 			RunLengthEncode(tt.input)
+		}
+	}
+}
+
+func BenchmarkDecode(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tt := range decodeTests {
+			RunLengthDecode(tt.input)
 		}
 	}
 }
